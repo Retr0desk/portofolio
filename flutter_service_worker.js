cbf16c1e9804bbe6f42a12844c97935de18f1d49
Retr0desk <118412877+Retr0desk@users.noreply.github.com>
2025-11-25 07:10:10 +0700

    Deploy update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0849c6f5e31f5777bfd72ba977b5a8f0
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "e0f0ab91664588b81403c6a4f5753e6b",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "1ac62c81ef5aa03eea24328ff0d7e79b",
+"flutter_bootstrap.js": "c6d6c527540b0dfb5f3d86a6c22d3ba4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
